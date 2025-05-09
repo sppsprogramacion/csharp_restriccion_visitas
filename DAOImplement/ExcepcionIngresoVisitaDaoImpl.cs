092ef759af5271234cec80569f6aaf39bf121bc6
Pedro <TuCorreo@example.com>
2025-05-09 13:19:47 -0300

    subida

--- DAOImplement/ExcepcionIngresoVisitaDaoImpl.cs
+++ DAOImplement/ExcepcionIngresoVisitaDaoImpl.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaDatos;
+using DAO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,21 @@ using System.Threading.Tasks;
 
 namespace DAOImplement
 {
-    internal class ExcepcionIngresoVisitaDaoImpl
+    public class ExcepcionIngresoVisitaDaoImpl : IExcepcionIngresoVisitaDao
     {
+        public Task<(DExcepcionIngresoVisita, string error)> BuscarExcepcionIngresoVisitaXId(int idExcepcionIngresoVisita)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(DExcepcionIngresoVisita, string error)> CrearExcepcionIngresoVisita(string excepcionIngresoVisita)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<(List<DExcepcionIngresoVisita>, string error)> RetornarExcepcionesIngresoXCiudadano(int idCiudadano)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
